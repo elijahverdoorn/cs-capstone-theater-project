87951c17389d9fc562c676b229e001c472540df0
Elijah Verdoorn <elijah@elijahverdoorn.com>
2017-09-13 22:21:23 -0500

    Add database creation and deletion scripts

---
+++ scripts/dropDatabase.sql
@@ -0,0 +1,5 @@
+DROP DATABASE IF NOT EXISTS theater_control_development;
+DROP DATABASE IF NOT EXISTS theater_control_test;
+DROP DATABASE IF NOT EXISTS theater_control_production;
+
+
---
+++ scripts/initDatabase.sql
@@ -0,0 +1,4 @@
+CREATE DATABASE IF NOT EXISTS theater_control_development;
+CREATE DATABASE IF NOT EXISTS theater_control_test;
+CREATE DATABASE IF NOT EXISTS theater_control_production;
+

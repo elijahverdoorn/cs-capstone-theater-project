e5a89bd6dab5290412ada073e0aeecc08ec97673
Elijah Verdoorn <elijah@elijahverdoorn.com>
2017-09-14 20:53:57 -0500

    Enable CORS

--- app.js
+++ app.js
@@ -10,6 +10,15 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(cookieParser())
 
+// Add headers
+app.use((req, res, next) => {
+	res.setHeader('Access-Control-Allow-Origin', '*')
+	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
+	res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type,Authorization')
+	res.setHeader('Access-Control-Allow-Credentials', true)
+	next()
+})
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
 	let err = new Error('Not Found')

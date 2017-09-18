e7cfd6e759488991dfc6cd77a64d1d0e16ee557c
Elijah Verdoorn <elijah@elijahverdoorn.com>
2017-09-18 10:57:39 -0500

    Move from native promises to async/await
    This syntax is cleaner

--- routes/action.js
+++ routes/action.js
@@ -3,66 +3,61 @@ import express from 'express'
 
 const router = express.Router()
 
-router.get('/:actionId?', (req, res) => {
-	models.Actions.findAll({
+router.get('/:actionId?', async (req, res) => {
+	let actions = await models.Actions.findAll({
 		where: {
 			id: (req.params.actionId == null) ? '*' : req.params.actionId
 		}
 	})
-	.then((action) => {
-		console.log('req')
-		res.send(action)
-	})
+	res.send(actions)
 })
 
-router.post('/', (req, res) => {
-	models.Actions.create({
+router.post('/', async (req, res) => {
+	let actions = await models.Actions.create({
 		address: req.query.address,
 		duration: req.query.duration,
 		name: req.query.name,
 		description: req.query.description
 	})
-	.then((action) => {
-		res.sendStatus(201)
-	})
 	.error(() => {
 		res.sendStatus(500)
 	})
+	res.sendStatus(201)
 })
 
-router.patch('/:actionId', (req, res) => {
-	models.Actions.find({
+router.patch('/:actionId', async (req, res) => {
+	let action = models.Actions.find({
 		where: {
 			id: req.params.actionId
 		}
-	})
-	.then((action) => {
-		if (!action) {
-			res.sendStatus(404)
-			return
-		}
-		action.address = req.query.address || action.address
-		action.duration = req.query.duration || action.duration
-		action.name = req.query.name || action.name
-		action.description = req.query.description || action.description
 	})
 	.error((err) => {
 		res.sendStatus(500)
 	})
+
+	if (!action) {
+		res.sendStatus(404)
+		return
+	}
+	action.address = req.query.address || action.address
+	action.duration = req.query.duration || action.duration
+	action.name = req.query.name || action.name
+	action.description = req.query.description || action.description
+	await action.save()
+	res.sendStatus(202)
 })
 
-router.delete('/:actionId', (req, res) => {
-	models.Actions.destroy({
+router.delete('/:actionId', async (req, res) => {
+	await models.Actions.destroy({
 		where: {
 			id: req.params.actionId
 		}
 	})
-	.then(() => {
-		res.sendStatus(202)
-	})
 	.error(() => {
 		res.sendStatus(500)
 	})
+
+	res.sendStatus(202)
 })
 
 module.exports = router

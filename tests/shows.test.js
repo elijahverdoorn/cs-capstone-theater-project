dfd29b7f93285032aa7aaafced2db84d4feb48c3
Elijah Verdoorn <elijah@elijahverdoorn.com>
2017-09-27 22:12:31 -0500

    Post actual data to /shows the endpoint

--- tests/shows.test.js
+++ tests/shows.test.js
@@ -5,5 +5,16 @@ describe('Test /shows route', () => {
 	test('It should respond to GET', async () => {
 		const response = await request(app).get('/shows')
 		expect(response.statusCode).toBe(200)
+	}),
+
+	test('It should respond to POST', async () => {
+		const response = await request(app)
+			.post('/shows')
+			.query({
+				name: 'Test show',
+				director: 'Elijah'
+			})
+		expect(response.statusCode).toBe(201)
 	})
+
 })

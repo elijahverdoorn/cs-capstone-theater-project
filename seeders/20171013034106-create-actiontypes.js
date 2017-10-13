8fa994e86b4597afb59c881c91b87481949d61d9
Elijah Verdoorn <elijah@elijahverdoorn.com>
2017-10-13 11:51:43 -0500

    Add seeder to insert actionTypes to database

---
+++ seeders/20171013034106-create-actiontypes.js
@@ -0,0 +1,36 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+		return queryInterface.bulkInsert('ActionTypes', [{
+			id: 1,
+			name: 'Show Image',
+			createdAt: new Date(),
+			updatedAt: new Date()
+		}, {
+			id: 2,
+			name: 'Change Background Color',
+			createdAt: new Date(),
+			updatedAt: new Date()
+		}, {
+			id: 3,
+			name: 'Play Video',
+			createdAt: new Date(),
+			updatedAt: new Date()
+		}, {
+			id: 4,
+			name: 'Play Sound',
+			createdAt: new Date(),
+			updatedAt: new Date()
+		}, {
+			id: 5,
+			name: 'Vibrate Phone',
+			createdAt: new Date(),
+			updatedAt: new Date()
+		}])
+	},
+
+  down: (queryInterface, Sequelize) => {
+		return queryInterface.bulkDelete('ActionTypes', null, {});
+  }
+};
